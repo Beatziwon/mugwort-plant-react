3c9c65ec547051023ee8de37d03274e3cbf4bf21
jw <beatziwon@gamil.com>
2026-06-11 15:33:07 +0900

    feat : 리액트 코드 변경 중

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { getRecommendedPlants, getRecommendText } from "./utils/recommend";
 import "./App.css";
+import { space } from "postcss/lib/list";
+
+const initialAnswers = {
+  space: null,            // 공간 크기
+  light: null,            // 빛 조건
+  experience: null,       // 식물 경험
+  water: null             // 물 조는 스타일
+}
+
+const stepOrder = ["statr", "space", "light", "experience", "water", "result"]
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [step, setStep] = useState("start");
+  const [answers, setAnswers] = useState(initialAnswers);
 
+  const select = (key, value, nextStep) => {
+    setAnswers((prev) => ({
+      ...prev,
+      [key] : value,
+    }));
+
+    setStep(nextStep);
+  }
+
+  const reset = () => {
+    setStep("start");
+    setAnswers(initialAnswers);
+  }
+
+  
   return (
-    <div>
-      <h1>머그워트 식물 추천</h1>
-      <p>React 프로젝트 시작!</p>
-    </div>
+    // <div>
+    //   <h1>머그워트 식물 추천</h1>
+    //   <p>React 프로젝트 시작!</p>
+    // </div>
   );
 }
 
